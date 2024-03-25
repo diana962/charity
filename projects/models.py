f3667cf2549a5f570e550026893088d9bccfd9e9
Anarbek4646 <londonisthecapita66@gmail.com>
2024-03-25 17:36:10 +0600

    same

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    documents = models.FileField(upload_to='')
+    amount = models.IntegerField()
+    status = models.BooleanField(default=False)
+    into = models.BooleanField(default=False)
+    photo = models.FileField(upload_to='')
+    category = models.CharField(max_length=255)
+    props = models.CharField(max_length=255)
+
